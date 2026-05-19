3903efbc07968f15b95fbaa5907fe76aaf5abe74
achrustowski <99372096+achrustowski@users.noreply.github.com>
2026-05-19 12:11:14 +0200

    Updates tile_map and tile_set for dev mode

---
+++ src/tile_map.h
@@ -0,0 +1,11 @@
+#ifndef TILE_MAP_H_
+#define TILE_MAP_H_
+
+#include "tile_map.h"
+
+void tile_map_init(Tile_Map* tile_map);
+void tile_map_reassign(Stage* s, int width, int height);
+void tile_map_assign(Tile_Map* tile_map, Tile_Set* tile_set);
+void tile_map_draw(Game* game, Tile_Map* tile_map, Tile_Set* tile_set);
+
+#endif
---
+++ src/tile_set.h
@@ -0,0 +1,9 @@
+#ifndef TILESET_H_
+#define TILESET_H_
+
+#include "structs.h"
+
+void tile_set_init(Tile_Set* tile_set);
+void tile_set_assign(Tile_Set* tile_set);
+
+#endif
